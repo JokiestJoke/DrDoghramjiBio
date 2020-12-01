5ac42a98ee926293bb9139943785caefc0e3ec35
JokiestJoke <mdoghram0@gmail.com>
2020-12-01 00:14:34 -0500

    added some more functionality

--- DOMevent.js
+++ DOMevent.js
@@ -2,10 +2,14 @@
 //My overall goal is to keep the global sphere low.
 
 //Global Delcarations
+// navbar declarations
 let home = document.getElementById("home");
 let about = document.getElementById("about");
 let patients = document.getElementById("patients");
 let sleep = document.getElementById("sleep");
+// body declarations
+let mainContentImg = document.getElementById("mainContentImage");
+let mainContentImgTxt = document.getElementById("imgText");
 
 
 
@@ -14,6 +18,7 @@ let hoverNavText = function(event){
     event.target.style.backgroundColor = "black";
     event.target.style.color = "white";
     event.target.style.fontWeight = "bold";
+    event.target.style.cursor = "pointer";
     // alert();
 };
 
@@ -21,8 +26,24 @@ let revertText = function(event){
     event.target.style.backgroundColor = "whitesmoke";
     event.target.style.color = "black";
     event.target.style.fontWeight = "normal";
+    event.target.style.cursor = "none";
 };
 
+let blurImg = function(event){
+    event.target.style.innerHTML = "Sample Text";
+    event.target.style.cursor = "pointer";
+    event.target.style.filter = "blur(2px)";
+    mainContentImgTxt.style.visibility = "visible";
+};
+
+let revertImgFilter = function(event){
+    let target = event.target;
+    event.target.style.filter = "blur(0px)";
+    event.target.style.cursor = "none";
+    if (target === mainContentImg){
+        mainContentImgTxt.style.visibility = "hidden";   
+    }
+};
 
 
 
@@ -39,6 +60,10 @@ patients.addEventListener("mouseout", revertText);
 
 sleep.addEventListener("mouseover", hoverNavText);
 sleep.addEventListener("mouseout", revertText);
+
+mainContentImg.addEventListener("mouseover", blurImg);
+mainContentImg.addEventListener("mouseout", revertImgFilter);
+
 
 
 
